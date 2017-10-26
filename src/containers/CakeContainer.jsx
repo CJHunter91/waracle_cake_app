6e6098ca9c2a90790e3ed7bfb9e5bf4b8dd97034
Chris Hunter <orkem2@gmail.com>
2017-10-26 20:12:08 +0100

    make ajax request to get cakeData and sets cakes data state

--- src/containers/CakeContainer.jsx
+++ src/containers/CakeContainer.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import CakeList from '../components/CakeList';
+import AjaxRequest from '../services/AjaxRequest'
 
 class CakeContainer extends Component {
-  render() {
-    return (
-      <CakeList/>
-    );
-  }
+
+	setCakeData(data){
+		this.setState({ cakeData:data })
+	}
+
+	componentWillMount(){
+		this.setState({ cakeData:[] })
+	}
+
+	componentDidMount(){
+		const ajaxRequest = new AjaxRequest("http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes")
+		ajaxRequest.get(this.setCakeData.bind(this))
+	}
+
+  	render() {
+    	return (
+      	<CakeList/>
+    	);
+  	}
 }
 
 export default CakeContainer;
