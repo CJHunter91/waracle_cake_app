773b67b356d7a58aa298449126e53a11398b8972
Chris Hunter <orkem2@gmail.com>
2017-10-27 09:34:30 +0100

    adds cake form container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import CakeContainer from './containers/CakeContainer'
+import CakeForm from './containers/CakeForm'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <CakeContainer/>
+    	<section id="main-view">
+     		<CakeContainer/>
+      		<CakeForm/>
+    	</section>
     );
   }
 }
---
+++ src/containers/CakeForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import CakeInput from '../components/CakeInput';
+
+class CakeForm extends Component {
+
+  	render() {
+    	return (
+      	<p>Test Cake Form</p>
+    	);
+  	}
+}
+
+export default CakeForm;
