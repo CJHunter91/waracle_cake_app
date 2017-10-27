dda40581c0a77edfc7b25137e963b41cbdae81ec
Chris Hunter <orkem2@gmail.com>
2017-10-27 10:04:42 +0100

    adds react-router-dom and create a route to form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import CakeContainer from './containers/CakeContainer'
-import CakeForm from './containers/CakeForm'
+import CakeContainer from './containers/CakeContainer';
+import CakeForm from './containers/CakeForm';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-    	<section id="main-view">
+    	<Router>
+
+    	<section id="app">
+    	<nav>
+    		<Link to={'/new'}>
+    		<li>Add Cake</li>
+    		</Link>
+    	</nav>
+    	<article id="main-view">
      		<CakeContainer/>
-      		<CakeForm/>
+     		<Route path='/new' component={CakeForm}/>
+    	</article>
     	</section>
+    	</Router>
     );
   }
 }
